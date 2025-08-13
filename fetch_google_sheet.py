0b9e3ccf24667a584f631127f92ab585e5f03a74
thode243 <rajueee243@gmail.com>
2025-08-13 16:30:25 +0530

    Create fetch_google_sheet.py

---
+++ fetch_google_sheet.py
@@ -0,0 +1,7 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open('YourSheetName').sheet1
