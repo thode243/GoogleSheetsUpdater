31cdae96dd10f259e1fa534661dbff208b67ab49
thode243 <rajueee243@gmail.com>
2025-08-17 07:16:37 +0530

    Update NSE_Option_Chain.py

--- NSE_Option_Chain.py
+++ NSE_Option_Chain.py
@@ -182,7 +182,7 @@ def is_market_open():
     now = datetime.now(ist)
     current_time = now.time()
     current_date = now.date()
-    market_start = dtime(3, 40)  # 9:15 AM IST
+    market_start = dtime(6, 40)  # 9:15 AM IST
     market_end = dtime(18, 20)  # 3:30 PM IST
     
     is_weekday = current_date.weekday() < 7
