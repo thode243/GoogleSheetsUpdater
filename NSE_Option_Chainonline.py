1507519a49cf8d51604e2447dec05d3c62c5e275
thode243 <rajueee243@gmail.com>
2025-09-29 09:19:30 +0530

    Update NSE_Option_Chainonline.py

--- NSE_Option_Chainonline.py
+++ NSE_Option_Chainonline.py
@@ -20,7 +20,7 @@ import pytz
 from time import sleep
 from zoneinfo import ZoneInfo 
 
-
+#
 # ===== CONFIG =====
 SHEET_ID = os.getenv("SHEET_ID", "15pghBDGQ34qSMI2xXukTYD4dzG2cOYIYmXfCtb-X5ow")
 CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH", "service_account.json")
@@ -163,6 +163,7 @@ if __name__ == "__main__":
             secs = seconds_until_next_open()
             logger.info(f"📉 Market closed, sleeping for {int(secs/60)} minutes until next open.")
             sleep(secs)
+
 
 
 
